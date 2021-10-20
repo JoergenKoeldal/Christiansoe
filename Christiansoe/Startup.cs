f5bd514d989c0054aac42999c7d4ae8a687bc69b
Joergen Koeldal <joer1894@stud.kea.dk>
2021-10-20 12:56:29 +0200

    Backend

--- Christiansoe/Startup.cs
+++ Christiansoe/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Christiansoe.Data;
 
 namespace Christiansoe
 {
@@ -32,6 +34,9 @@ namespace Christiansoe
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Christiansoe", Version = "v1" });
             });
+
+            services.AddDbContext<ChristiansoeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ChristiansoeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
